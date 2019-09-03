9b9dbaf8fe2853199c83d384c1c922a391431360
Elvira Fuente Romero <elvira.fuente@ultimasoftware.com>
2019-09-03 11:10:13 +0200

    add custom call api hook with fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { useCall } from './hooks/call'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        hola
-      </header>
-    </div>
-  );
+export default function App() {
+    // const [fetchedData, setFetchedData] = useState(null)
+    const ApiEndPoint = 'https://reqres.in/api/users'
+    const [fetchedData, ] = useCall(ApiEndPoint, [])
+    
+    console.log(fetchedData)
+
+    // useEffect(() => {
+    //     fetch(ApiEndPoint)
+    //     .then(res => res.json())
+    //     .then(responseData =>{
+    //         console.log(responseData)
+    //         setFetchedData(responseData.data)
+    //     }).catch((error)=>alert(error))
+    // }, []);
+
+    return (
+        <div className="App">
+            <header className="App__header">
+                <h1>Users Database</h1>
+            </header>
+            { fetchedData 
+                ? 
+                    <main className="container">
+                        <ul className="card__list">
+                            {fetchedData.map(item =>{
+                                return (
+                                    <li key={item.id} className="list-group-item card">
+                                        <img src={item.avatar} alt={`${item.first_name} ${item.last_name}`} className="card__avatar" />
+                                        <div className="card__info">
+                                            <h3>{`${item.first_name} ${item.last_name}`}</h3>
+                                            <p>{item.email}</p>
+                                        </div>
+                                        
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </main> 
+                : <main><p>Loading...</p></main>
+            }
+        </div>
+    );
 }
 
-export default App;
